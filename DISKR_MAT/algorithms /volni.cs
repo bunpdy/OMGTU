e72eb32cbfb03f401f1619fffa9804fa7ba32d9f
bunpdy <100338412+bunpdy@users.noreply.github.com>
2024-04-17 14:32:55 +0600

    Update volni.cs

--- DISKR_MAT/algorithms /volni.cs
+++ DISKR_MAT/algorithms /volni.cs
@@ -9,19 +9,36 @@ ls = [[-1, -1, -1, -1, -1, -1, -1, -1],
       [-1, inf, inf, inf, inf, inf, inf, -1],
       [-1, -1, -1, -1, -1, -1, -1, -1]]
     
+def func(center_x, center_y):
+    if ls[center_x - 1][center_y - 1] != -1:
+        ls[center_x - 1][center_y - 1] = min(ls[center_x - 1][center_y - 1], ls[i][j] + 1)
+        
+    if ls[center_x - 1][center_y] != -1:
+        ls[center_x - 1][center_y] = min(ls[center_x - 1][center_y], ls[i][j] + 1)
+        
+    if ls[center_x - 1][center_y + 1] != -1:
+        ls[center_x - 1][center_y + 1] = min(ls[center_x - 1][center_y + 1], ls[i][j] + 1)
+        
+    if ls[center_x][center_y - 1] != -1:
+        ls[center_x][center_y - 1] = min(ls[center_x][center_y - 1], ls[i][j] + 1)
+        
+    if ls[center_x][center_y + 1] != -1:
+        ls[center_x][center_y + 1] = min(ls[center_x][center_y + 1], ls[i][j] + 1)
+        
+    if ls[center_x + 1][center_y - 1] != -1:
+        ls[center_x + 1][center_y - 1] = min(ls[center_x + 1][center_y - 1], ls[i][j] + 1)
+        
+    if ls[center_x + 1][center_y] != -1:
+        ls[center_x + 1][center_y] = min(ls[center_x + 1][center_y], ls[i][j] + 1)
+        
+    if ls[center_x + 1][center_y + 1] != -1:
+        ls[center_x + 1][center_y + 1] = min(ls[center_x + 1][center_y + 1], ls[i][j] + 1)
 
     
-x_s, y_s = 0, 0
-for l in range(1):
-    for i in range(1, len(ls) - 1):
-        for j in range(1, len(ls) - 1):
-            around = [i - 1, j - 1, i - 1, j, i - 1, j + 1, i, j - 1, i, j + 1, i + 1, j - 1, i + 1, j, i + 1, j + 1]
-            for t in range(len(around) - 1):
-                if (t % 2 == 0) and (ls[around[t]][around[t + 1]] != -1):
-                    x1 = around[t]
-                    y1 = around[t + 1]
-                    if ls[i][j] + 1 < ls[x1][y1]:
-                        ls[x1][y1] = ls[i][j] + 1
+for i in range(1, len(ls) - 1):
+    for j in range(1, len(ls) - 1):
+        func(i, j)
+                
         
 
             #if (t % 2 == 0) and (ls[around[t], around[t+1]] != -1):
