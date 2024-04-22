d56ce0f9e739258ac0f75c2f38548e0fae452d90
bunpdy <100338412+bunpdy@users.noreply.github.com>
2024-04-22 14:27:54 +0600

    Update Dijkstra's.py

--- DISKR_MAT/algorithms /Dijkstra's.py
+++ DISKR_MAT/algorithms /Dijkstra's.py
@@ -6,14 +6,35 @@ matrix = [[0, 10, 30, 50, 10],
           [Inf, 40, 20, 0, Inf],
           [10, Inf, 10, 30, 0]]
 
-ls = [0, Inf, Inf, Inf, Inf]  # метки вершин от которых смотрим пути
 
-used_vertex = []  # вершины которые мы рассмотрели
+ls = [Inf, Inf, Inf, Inf, Inf]  # метки вершин от которых смотрим пути
+
+from_point = int(input("Введите номер начальной вершины: "))
+
+to_point = int(input("Введите номер конечной вершины: "))
+
+ls.pop(from_point)
+ls.insert(from_point, 0)
 
 for i in range(len(matrix)):
     for j in range(len(matrix[i])):
-        if (ls[i] + matrix[i][j] < ls[j]) and (i not in used_vertex):
+        if ls[i] + matrix[i][j] < ls[j]:
             ls[j] = ls[i] + matrix[i][j]
-    used_vertex.append(i)
 
-print(ls)
+
+p = to_point
+vertex = [p]
+while p != from_point:
+    flag = False
+    for i in range(len(matrix)):
+        if flag:
+            break
+        for j in range(len(matrix[i])):
+            if (j == p) and (i not in vertex):
+                if ls[i] + matrix[i][j] == ls[p]:
+                    vertex.append(i)
+                    p = i
+                    if p == from_point:
+                        flag = True
+
+print(vertex[::-1], "Путь включает в себя вершину старта и конца")
